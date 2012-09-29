dfa63cdec445e684adf1a6ea6ab9f2f250f951d6
Adrian Lee <jun.irok@gmail.com>
2012-09-29 17:54:39 -0400

    fish

--- enemies.js
+++ enemies.js
@@ -21,30 +21,81 @@ function Bullet(x,y){
 
     function setVelocity(x,y,speed){
         var theta = Math.atan(y/x);
-        this.worldBody.SetLinearVelocity(   
+        this.worldBody.SetLinearVelocity(
             new Box2D.Common.Math.b2Vec2(
                 speed*Math.cos(x),
                 speed*Math.sin(y) ));
         return this;
     }
-  //  this.createBody = createBody;
+
+    // this.createBody = createBody;
     this.setVelocity = setVelocity;
     this.params = getParams();
     return this;
 };
 
-
 Bullet.prototype = {
-
-
-    createBody: function(world,params){  
-        var p = params||this.params;    
+    createBody: function(world,params){
+        var p = params||this.params;
         return Bullet.superclass.createBody.call(this,world,p);
-
     }
-
 };
 extend(Bullet,CAAT.B2DPolygonBody);
+
+
+// Fish
+function Fish(x, y) {
+    Fish.superclass.constructor.call(this);
+
+    var params= {
+        bodyType:   Box2D.Dynamics.b2Body.b2_dynamicBody,
+        x: x || 100,
+        y: y || 100,
+        density: 0.4,
+        friction: 0,
+        restitution: 1,
+        polygonType: CAAT.B2DPolygonBody.Type.BOX,
+        image: new CAAT.SpriteImage().initialize(document.getElementById('sprite_1'), 1, 1),
+        bodyDef:    [
+            {x: 0,  y: 0 },
+            {x: 20,  y: 30 }
+        ],
+        userData: {}
+    };
+
+    function getParams(){
+        return params;
+    }
+
+    function setVelocity(x,y,speed){
+        var theta = Math.atan(y/x);
+        this.worldBody.SetLinearVelocity(
+            new Box2D.Common.Math.b2Vec2(
+                speed*Math.cos(x),
+                speed*Math.sin(y) ));
+        return this;
+    }
+
+    this.setBackgroundImage( new CAAT.SpriteImage().initialize(document.getElementById('sprite_1'), 1, 1), true );
+    this.setAnimationImageIndex( [0,1,2,1] );
+
+
+    // this.createBody = createBody;
+    this.setVelocity = setVelocity;
+    this.params = getParams();
+    return this;
+};
+
+Fish.prototype = {
+    createBody: function(world,params){
+        var p = params ||this.params;
+        return Fish.superclass.createBody.call(this,world,p);
+    }
+};
+extend(Fish, CAAT.B2DPolygonBody);
+
+
+
 
 
 
