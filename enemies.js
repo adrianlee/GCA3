394d77ec0ade1d3ec560e152387b80d44a491bb2
Adrian Lee <jun.irok@gmail.com>
2012-09-28 23:31:09 -0400

    Added startLevel()

--- enemies.js
+++ enemies.js
@@ -1,3 +1,25 @@
 function Bullet(){
-  
+
 }
+
+function enemy1() {
+
+}
+
+        function enemy1(x1, y1, x2, y2, delay, speed) {
+            return new CAAT.Actor().
+                setFillStyle( 'blue' ).
+                setBounds(-20, -20, 20, 10).
+                setDiscardable(true).
+                // cacheAsBitmap().
+                addBehavior(
+                    new CAAT.PathBehavior().
+                        setValues(
+                            new CAAT.Path().
+                                setLinear( x1, y1, x2, y2 )
+                        ).
+                        setAutoRotate(true).
+                        setFrameTime( delay, speed ).
+                        setCycle( false )
+                );
+        }
