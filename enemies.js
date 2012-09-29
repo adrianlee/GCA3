54e65c5fb5c39d0d53b72862c72cea27e73e5e7d
Xavier Denis <xldenis@gmail.com>
2012-09-29 03:50:59 -0400

    Work on Bullet class. Implement enemies as Box2d entities

--- enemies.js
+++ enemies.js
@@ -1,7 +1,64 @@
-function Bullet(){
+function Bullet(x,y){
+    Bullet.superclass.constructor.call(this);
+        var params= {
+        bodyType:   Box2D.Dynamics.b2Body.b2_dynamicBody,
+        x: x||100,
+        y: y||100,
+        density:    1.2,
+        friction:   1,
+        restitution:0.5,
+        polygonType:CAAT.B2DPolygonBody.Type.BOX,
+        bodyDef:    [
+            {x: 0,  y: 0 },
+            {x: 10,  y: 10 }
+        ],
+        userData:   {}
+    };
 
-}
+    function getParams(){
+        console.log("as");
+        return params;
+    }
+    function createBody(world,params){
+        return Bullet.superclass.createBody(world,params);
+    }
+    function setVelocity(x,y){
+        this.worldBody.SetLinearVelocity(
+            new Box2D.Common.Math.b2Vec2(
+                x,
+                y ));
+        return this;
+    }
+  //  this.createBody = createBody;
+    this.setVelocity = setVelocity;
+    this.params = getParams();
+    return this;
+};
+extend(Bullet,CAAT.B2DPolygonBody);
+/*
+Bullet.prototype = {
 
+    createBody: function(world,params){
+        var test = this.params;  
+        Bullet.superclass.createBody(world,this.params);
+        var velocity= {
+            x: 0,
+            y: Math.random()
+        };
+        this.worldBody.SetLinearVelocity(
+        new Box2D.Common.Math.b2Vec2(
+                velocity.x,
+                velocity.y) );
+        return this;
+    }
+};
+extend(Bullet,CAAT.B2DPolygonBody);
+
+
+function Bullet(world,x,y){
+
+    return new CAAT.B2DPolygonBody()
+}*/
 function enemy1() {
 
 }
