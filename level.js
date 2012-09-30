139713d203e0be5b1af5746506a271e3b0b3239c
Nick Morrill <nmorrill2@gmail.com>
2012-09-30 10:36:58 -0600

    Added more levels
    It made me update it

--- level.js
+++ level.js
@@ -4,7 +4,121 @@ var sample = {
     enemy: [
         {
             type: 0,
+            amount: 2,
+            interporlation: 0
+        },
+        {
+            type: 1,
             amount: 1,
+            interpolation: 0
+        },
+        {
+            type: 2,
+            amount: 0,
+            interpolation: 0
+        },
+        {
+            type: 3,
+            amount: 0,
+            interpolation: 0
+        },
+        {
+            type: 4,
+            amount: 0,
+            interpolation: 0
+        }
+    ],
+    background: 'tile',
+    music: 'main',
+    powerup: 5
+};
+
+var sample2 = {
+    enemy: [
+        {
+            type: 0,
+            amount: 0,
+            interporlation: 0
+        },
+        {
+            type: 1,
+            amount: 3,
+            interpolation: 0
+        }
+    ]
+};
+
+var sample3 = {
+    enemy: [
+        {
+            type: 0,
+            amount: 0,
+            interporlation: 0
+        },
+        {
+            type: 1,
+            amount: 0,
+            interpolation: 0
+        },
+        {
+            type: 2,
+            amount: 2,
+            interpolation: 0
+        },
+        {
+            type: 3,
+            amount: 2,
+            interpolation: 0
+        },
+        {
+            type: 4,
+            amount: 0,
+            interpolation: 0
+        }
+    ],
+    background: 'tile',
+    music: 'main',
+    powerup: 5
+};
+
+var sample4 = {
+    enemy: [
+        {
+            type: 0,
+            amount: 2,
+            interporlation: 0
+        },
+        {
+            type: 1,
+            amount: 0,
+            interpolation: 0
+        },
+        {
+            type: 2,
+            amount: 2,
+            interpolation: 0
+        },
+        {
+            type: 3,
+            amount: 1,
+            interpolation: 0
+        },
+        {
+            type: 4,
+            amount: 1,
+            interpolation: 0
+        }
+    ],
+    background: 'tile',
+    music: 'main',
+    powerup: 5
+};
+
+var sample5 = {
+    enemy: [
+        {
+            type: 0,
+            amount: 0,
             interporlation: 0
         },
         {
@@ -32,8 +146,72 @@ var sample = {
     music: 'main',
     powerup: 5
 };
+var sample6 = {
+    enemy: [
+        {
+            type: 0,
+            amount: 2,
+            interporlation: 0
+        },
+        {
+            type: 1,
+            amount: 2,
+            interpolation: 0
+        },
+        {
+            type: 2,
+            amount: 1,
+            interpolation: 0
+        },
+        {
+            type: 3,
+            amount: 1,
+            interpolation: 0
+        },
+        {
+            type: 4,
+            amount: 1,
+            interpolation: 0
+        }
+    ],
+    background: 'tile',
+    music: 'main',
+    powerup: 5
+};
+var sample7 = {
+    enemy: [
+        {
+            type: 0,
+            amount: 0,
+            interporlation: 0
+        },
+        {
+            type: 1,
+            amount: 3,
+            interpolation: 0
+        },
+        {
+            type: 2,
+            amount: 0,
+            interpolation: 0
+        },
+        {
+            type: 3,
+            amount: 0,
+            interpolation: 0
+        },
+        {
+            type: 4,
+            amount: 2,
+            interpolation: 0
+        }
+    ],
+    background: 'tile',
+    music: 'main',
+    powerup: 5
+};
 
-var sample2 = {
+var sample8 = {
     enemy: [
         {
             type: 0,
@@ -42,11 +220,107 @@ var sample2 = {
         },
         {
             type: 1,
-            amount: 8,
+            amount: 0,
+            interpolation: 0
+        },
+        {
+            type: 2,
+            amount: 1,
+            interpolation: 0
+        },
+        {
+            type: 3,
+            amount: 2,
+            interpolation: 0
+        },
+        {
+            type: 4,
+            amount: 2,
             interpolation: 0
         }
-    ]
+    ],
+    background: 'tile',
+    music: 'main',
+    powerup: 5
+};
+var sample9 = {
+    enemy: [
+        {
+            type: 0,
+            amount: 0,
+            interporlation: 0
+        },
+        {
+            type: 1,
+            amount: 0,
+            interpolation: 0
+        },
+        {
+            type: 2,
+            amount: 6,
+            interpolation: 0
+        },
+        {
+            type: 3,
+            amount: 1,
+            interpolation: 0
+        },
+        {
+            type: 4,
+            amount: 0,
+            interpolation: 0
+        }
+    ],
+    background: 'tile',
+    music: 'main',
+    powerup: 5
 };
 
+var sample10 = {
+    enemy: [
+        {
+            type: 0,
+            amount: 10,
+            interporlation: 0
+        },
+        {
+            type: 1,
+            amount: 0,
+            interpolation: 0
+        },
+        {
+            type: 2,
+            amount: 0,
+            interpolation: 0
+        },
+        {
+            type: 3,
+            amount: 0,
+            interpolation: 0
+        },
+        {
+            type: 4,
+            amount: 0,
+            interpolation: 0
+        }
+    ],
+    background: 'tile',
+    music: 'main',
+    powerup: 5
+};
+
+
+
+
+
+
+levelJSON.push(sample9);
+levelJSON.push(sample10);
 levelJSON.push(sample);
 levelJSON.push(sample2);
+levelJSON.push(sample3);
+levelJSON.push(sample4);
+levelJSON.push(sample5);
+levelJSON.push(sample6);
+levelJSON.push(sample7);
+levelJSON.push(sample8);
