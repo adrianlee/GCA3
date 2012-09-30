f244bc86650fe7a67a16a7fbe7a12e743c3e124a
Nick Morrill <nmorrill2@gmail.com>
2012-09-30 11:48:32 -0600

    added levels

--- level.js
+++ level.js
@@ -309,13 +309,339 @@ var sample10 = {
     powerup: 5
 };
 
+var sample11 = {
+    enemy: [
+        {
+            type: 0,
+            amount: 4,
+            interporlation: 0
+        },
+        {
+            type: 1,
+            amount: 1,
+            interpolation: 0
+        },
+        {
+            type: 2,
+            amount: 0,
+            interpolation: 0
+        },
+        {
+            type: 3,
+            amount: 2,
+            interpolation: 0
+        },
+        {
+            type: 4,
+            amount: 0,
+            interpolation: 0
+        }
+    ],
+    background: 'tile',
+    music: 'main',
+    powerup: 5
+};
+
+
+var sample12 = {
+    enemy: [
+        {
+            type: 0,
+            amount: 0,
+            interporlation: 0
+        },
+        {
+            type: 1,
+            amount: 1,
+            interpolation: 0
+        },
+        {
+            type: 2,
+            amount: 0,
+            interpolation: 0
+        },
+        {
+            type: 3,
+            amount: 0,
+            interpolation: 0
+        },
+        {
+            type: 4,
+            amount: 5,
+            interpolation: 0
+        }
+    ],
+    background: 'tile',
+    music: 'main',
+    powerup: 5
+};
+
+var sample13 = {
+    enemy: [
+        {
+            type: 0,
+            amount: 3,
+            interporlation: 0
+        },
+        {
+            type: 1,
+            amount: 0,
+            interpolation: 0
+        },
+        {
+            type: 2,
+            amount: 2,
+            interpolation: 0
+        },
+        {
+            type: 3,
+            amount: 2,
+            interpolation: 0
+        },
+        {
+            type: 4,
+            amount: 1,
+            interpolation: 0
+        }
+    ],
+    background: 'tile',
+    music: 'main',
+    powerup: 5
+};
+
+var sample14 = {
+    enemy: [
+        {
+            type: 0,
+            amount: 0,
+            interporlation: 0
+        },
+        {
+            type: 1,
+            amount: 0,
+            interpolation: 0
+        },
+        {
+            type: 2,
+            amount: 8,
+            interpolation: 0
+        },
+        {
+            type: 3,
+            amount: 0,
+            interpolation: 0
+        },
+        {
+            type: 4,
+            amount: 0,
+            interpolation: 0
+        }
+    ],
+    background: 'tile',
+    music: 'main',
+    powerup: 5
+};
+
+var sample15 = {
+    enemy: [
+        {
+            type: 0,
+            amount: 2,
+            interporlation: 0
+        },
+        {
+            type: 1,
+            amount: 0,
+            interpolation: 0
+        },
+        {
+            type: 2,
+            amount: 0,
+            interpolation: 0
+        },
+        {
+            type: 3,
+            amount: 4,
+            interpolation: 0
+        },
+        {
+            type: 4,
+            amount: 0,
+            interpolation: 0
+        }
+    ],
+    background: 'tile',
+    music: 'main',
+    powerup: 5
+};
+
+var sample16 = {
+    enemy: [
+        {
+            type: 0,
+            amount: 0,
+            interporlation: 0
+        },
+        {
+            type: 1,
+            amount: 0,
+            interpolation: 0
+        },
+        {
+            type: 2,
+            amount: 2,
+            interpolation: 0
+        },
+        {
+            type: 3,
+            amount: 0,
+            interpolation: 0
+        },
+        {
+            type: 4,
+            amount: 5,
+            interpolation: 0
+        }
+    ],
+    background: 'tile',
+    music: 'main',
+    powerup: 5
+};
+
+var sample17 = {
+    enemy: [
+        {
+            type: 0,
+            amount: 3,
+            interporlation: 0
+        },
+        {
+            type: 1,
+            amount: 2,
+            interpolation: 0
+        },
+        {
+            type: 2,
+            amount: 2,
+            interpolation: 0
+        },
+        {
+            type: 3,
+            amount: 1,
+            interpolation: 0
+        },
+        {
+            type: 4,
+            amount: 0,
+            interpolation: 0
+        }
+    ],
+    background: 'tile',
+    music: 'main',
+    powerup: 5
+};
+
+var sample18 = {
+    enemy: [
+        {
+            type: 0,
+            amount: 8,
+            interporlation: 0
+        },
+        {
+            type: 1,
+            amount: 3,
+            interpolation: 0
+        },
+        {
+            type: 2,
+            amount: 1,
+            interpolation: 0
+        },
+        {
+            type: 3,
+            amount: 0,
+            interpolation: 0
+        },
+        {
+            type: 4,
+            amount: 0,
+            interpolation: 0
+        }
+    ],
+    background: 'tile',
+    music: 'main',
+    powerup: 5
+};
+
+var sample19 = {
+    enemy: [
+        {
+            type: 0,
+            amount: 1,
+            interporlation: 0
+        },
+        {
+            type: 1,
+            amount: 3,
+            interpolation: 0
+        },
+        {
+            type: 2,
+            amount: 3,
+            interpolation: 0
+        },
+        {
+            type: 3,
+            amount: 0,
+            interpolation: 0
+        },
+        {
+            type: 4,
+            amount: 0,
+            interpolation: 0
+        }
+    ],
+    background: 'tile',
+    music: 'main',
+    powerup: 5
+};
+
+var sample20 = {
+    enemy: [
+        {
+            type: 0,
+            amount: 5,
+            interporlation: 0
+        },
+        {
+            type: 1,
+            amount: 5,
+            interpolation: 0
+        },
+        {
+            type: 2,
+            amount: 5,
+            interpolation: 0
+        },
+        {
+            type: 3,
+            amount: 5,
+            interpolation: 0
+        },
+        {
+            type: 4,
+            amount: 5,
+            interpolation: 0
+        }
+    ],
+    background: 'tile',
+    music: 'main',
+    powerup: 5
+};
 
 
 
-
-
-levelJSON.push(sample9);
-levelJSON.push(sample10);
 levelJSON.push(sample);
 levelJSON.push(sample2);
 levelJSON.push(sample3);
@@ -324,3 +650,15 @@ levelJSON.push(sample5);
 levelJSON.push(sample6);
 levelJSON.push(sample7);
 levelJSON.push(sample8);
+levelJSON.push(sample9);
+levelJSON.push(sample10);
+levelJSON.push(sample11);
+levelJSON.push(sample12);
+levelJSON.push(sample13);
+levelJSON.push(sample14);
+levelJSON.push(sample15);
+levelJSON.push(sample16);
+levelJSON.push(sample17);
+levelJSON.push(sample18);
+levelJSON.push(sample19);
+levelJSON.push(sample20);
